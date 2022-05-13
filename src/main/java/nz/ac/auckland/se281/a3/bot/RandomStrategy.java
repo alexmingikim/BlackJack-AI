ff223aab7d9452e0e268f184c799239da7765f19
Alex <mkim897@aucklanduni.ac.nz>
2022-05-13 21:37:04 +1200

    Implemented play() and bet() methods of RandomStrategy class as required

--- src/main/java/nz/ac/auckland/se281/a3/bot/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/bot/RandomStrategy.java
@@ -1,17 +1,35 @@
 package nz.ac.auckland.se281.a3.bot;
 
+import java.util.Random;
+
 import nz.ac.auckland.se281.a3.Participant.Action;
 
 public class RandomStrategy implements Strategy {
 
 	@Override
 	public Action play() {
-		return null;
+		// Generate random float between 0.0 and 1.0
+		Random random = new Random();
+		float numberRandom = random.nextFloat();
+
+		Action resultAction;
+		// Half of time --> HIT
+		if (numberRandom < 0.5f) {
+			resultAction = Action.HIT;
+		}
+		// Other half of time --> HOLD
+		else {
+			resultAction = Action.HOLD;
+		}
+		return resultAction;
 	}
 
 	@Override
 	public int bet() {
-		return 0;
+		// Create a random between 1 and 100 (inclusive)
+		Random random = new Random();
+		int numberRandom = random.nextInt(1, 101);
+		return numberRandom;
 	}
 
 }
